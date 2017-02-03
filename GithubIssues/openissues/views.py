7445651804e73c2a95e172719fc69e4aff958362
Alen Thomas <alenthomas9@gmail.com>
2017-02-03 21:40:48 +0530

    adds form validation in views and templates

--- GithubIssues/openissues/views.py
+++ GithubIssues/openissues/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
+INVALID_ERROR = {'error':True, 'message':"Invalid url, Please try again"}
+
 @method_decorator(csrf_exempt, name='dispatch')
 class HomePage(View):
 
@@ -10,4 +12,17 @@ class HomePage(View):
         return render(request, 'openissues/home.html')
 
     def post(self, request):
+        link = request.POST.get('url', None)
+        url_list = link.split('/')
+        try:
+            for words in url_list:
+                if 'github.com' in words:
+                    index = url_list.index(words)
+            owner, repo = url_list[index+1], url_list[index+2]
+        except UnboundLocalError:
+            return self.error(request, INVALID_ERROR)
+
         return render(request, 'openissues/home.html')
+
+    def error(self, request, message):
+        return render(request, 'openissues/home.html', message)
