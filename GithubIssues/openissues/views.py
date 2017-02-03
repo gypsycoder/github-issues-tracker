eff72f31211b4f98388b7bcb61b7c6a5ed411cb4
Alen Thomas <alenthomas9@gmail.com>
2017-02-03 21:26:10 +0530

    adds methods for csrf_exempt

--- GithubIssues/openissues/views.py
+++ GithubIssues/openissues/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
+@method_decorator(csrf_exempt, name='dispatch')
 class HomePage(View):
 
     def get(self, request):
+        return render(request, 'openissues/home.html')
+
+    def post(self, request):
         return render(request, 'openissues/home.html')
