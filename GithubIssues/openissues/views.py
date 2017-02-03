e1c39f94a65d176c01569f9f7a86c807e05b9c1d
Alen Thomas <alenthomas9@gmail.com>
2017-02-03 21:58:51 +0530

    perform api query and adds json response to list

--- GithubIssues/openissues/views.py
+++ GithubIssues/openissues/views.py
@@ -3,7 +3,11 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
+import datetime, requests, json
+
 INVALID_ERROR = {'error':True, 'message':"Invalid url, Please try again"}
+
+API = "https://api.github.com/repos/"
 
 @method_decorator(csrf_exempt, name='dispatch')
 class HomePage(View):
@@ -21,6 +25,22 @@ class HomePage(View):
             owner, repo = url_list[index+1], url_list[index+2]
         except UnboundLocalError:
             return self.error(request, INVALID_ERROR)
+
+        api = API + owner + '/' + repo + '/issues?status=open&page='
+        open_issues, count = 0, 0
+
+        issues_list = []
+
+        while(True):
+            count += 1
+            final_url = api + str(count)
+            print(final_url)
+            api_response = requests.get(final_url)
+            data = json.loads(api_response.text)
+            if(len(data) == 0):
+                break
+            open_issues += len(data)
+            issues_list.extend(data)
 
         return render(request, 'openissues/home.html')
 
